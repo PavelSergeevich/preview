7531af20c96cf95548eeb779f9dc7883bd979b0d
Pavlo Dubynka <pavlo@MacBook-Air-Khrystyna.local>
2022-01-04 12:36:31 +0200

    google-tag-fimal-fix

--- components/Head.js
+++ components/Head.js
@@ -8,8 +8,55 @@ const Head = ({ title, description }) => (
       <title>{title || ""}</title>
       <meta name="description" content={description || ""} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="google-site-verification" content="3oun9ZZztq5VpDD1Lp73V5qsF3JZh1ah47vVOq3j9FM" />
-      <Script
+      <meta name="google-site-verification" content="3oun9ZZztq5VpDD1Lp73V5qsF3JZh1ah47vVOq3j9FM" />      
+    <Script
+      async
+      id="site-tag-aw"
+      src="https://www.googletagmanager.com/gtag/js?id=AW-1002121066"
+    />
+    <Script
+      id="site-tag"
+      dangerouslySetInnerHTML={{
+        __html: `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'AW-1002121066');`,
+      }}
+    />
+    <Script
+      id="event-snippet"
+      dangerouslySetInnerHTML={{
+        __html: `gtag('event', 'conversion', {'send_to': 'AW-1002121066/XCOaCOfutGAQ6s7s3QM'});`,
+      }}
+    />
+    <Script
+      async
+      id="site-tag-analytics"
+      src="https://www.googletagmanager.com/gtag/js?id=G-L3M7YL6FVR"
+    />
+    <Script
+      id="site-tag-gtag"
+      dangerouslySetInnerHTML={{
+        __html: `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-L3M7YL6FVR');`,
+      }}
+    />
+    <Script
+      id="facebook-pixel"
+      dangerouslySetInnerHTML={{
+        __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1049503675834921');
+        fbq('track', 'PageView');`,
+      }}
+    />  
+    <Script
       id="tag-manager"
       dangerouslySetInnerHTML={{
         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -17,21 +64,6 @@ const Head = ({ title, description }) => (
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-52Z565Q');`,
-      }}
-    />
-    <Script
-      async
-      id="global-aw"
-      src="https://www.googletagmanager.com/gtag/js?id=AW-1002121066"
-    />
-    <Script
-      id="google-global"
-      dangerouslySetInnerHTML={{
-        __html: `window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-      
-        gtag('config', 'AW-1002121066');`,
       }}
     />    
     <link rel="apple-touch-icon" sizes="180x180" href="/touch-icon-iphone.png"></link>
--- components/Page.js
+++ components/Page.js
@@ -4,47 +4,24 @@ import Script from "next/script";
 
 const Page = ({ blok }) => (
   <SbEditable content={blok}>
-    <Script
-      async
-      id="global"
-      src="https://www.googletagmanager.com/gtag/js?id=G-L3M7YL6FVR"
-    />
     <noscript>
       <iframe
-        title="tag-manager"
         src="https://www.googletagmanager.com/ns.html?id=GTM-52Z565Q"
         height="0"
         width="0"
         style="display:none;visibility:hidden"
       ></iframe>
     </noscript>
-    <Script
-      id="fb-pixel"
-      dangerouslySetInnerHTML={{
-        __html: `${blok.script_fb}`,
-      }}
-    />
-    <Script
-      id="global-new"
-      dangerouslySetInnerHTML={{
-        __html: `${blok.script_fb_ns}`,
-      }}
-    />
-     <Script
-      id="event-tag"
-      dangerouslySetInnerHTML={{
-        __html: `${blok.script_event}`,
-      }}
-    />
+
     <noscript>
       <img
         height="1"
         width="1"
-        style={{ display: "none" }}
+        style="display:none"
         src="https://www.facebook.com/tr?id=1049503675834921&ev=PageView&noscript=1"
-        alt="fb-pixel"
       />
-    </noscript>   
+    </noscript>
+
     <main>
       {blok.body
         ? blok.body.map((blok) => (
