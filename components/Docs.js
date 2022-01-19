bdf712f9f5f86faaae5a287c9aaa88a1ad4a77ae
Pavlo Dubynka <pavlo@MacBook-Air-Khrystyna.local>
2022-01-19 12:59:09 +0200

    pdf-iframe

--- components/Docs.js
+++ components/Docs.js
@@ -5,21 +5,31 @@ import SbEditable from "storyblok-react";
 const Docs = ({ blok }) => {
   return (
     <SbEditable content={blok} key={blok._uid}>
-      <Container>
-        <Row>
+      
           <div className="bd-desc">
-            <b className="h5">{blok.title}</b>
+            <b className="h5 mt-3">{blok.title}</b>
           </div>
           <div className="mb-4">
-            <embed
+            <iframe
               src={blok.document.filename}
-              type="application/pdf"
-              width="100%"
-              height="600px"
-            />
+              frameBorder="0"
+              scrolling="auto"
+              height={800}
+              width={884}
+            ></iframe>
+            <div className="btn-case">
+            <a
+            className="btn-blind"
+              href={blok.document.filename}
+              alt="alt text"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {blok.doc_link}
+            </a>
+            </div>
           </div>
-        </Row>
-      </Container>
+        
     </SbEditable>
   );
 };
